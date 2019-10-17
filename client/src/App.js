1d877ab98964140fab463bdc0d736627f171bd70
Tim Dill <brodt7258@gmail.com>
2019-10-17 16:34:44 -0400

    server-side uploads

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const [image, setImage] = useState(null);
+  const inputRef = useRef(null);
 
   const handleSelect = e => {
     setImage(e.target.files[0])
+    //e.target.value = null;
     //console.log(e.target.files[0])
   };
 
-  const handleSubmit = async (e) => {
+  const handleLocalSubmit = async (e) => {
     e.preventDefault();
     console.log(image);
     const data = new FormData();
@@ -22,21 +24,30 @@ function App() {
       data
     );
     console.log('RESPONSE', res);
+    inputRef.current.value = null;
+  };
+
+  const handleSignedSubmit = async (e) => {
+
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Cloudinary Demo</h2>
-        <form onSubmit={handleSubmit}>
+        <form>
           <label htmlFor="image">Image</label>
           <input 
             type="file" 
             name="image" 
             onChange={handleSelect}
+            ref={inputRef}
           />
-          <button type="submit">
-            Submit
+          <button onClick={handleLocalSubmit}>
+            Submit unsigned
+          </button>
+          <button onClick={handleSignedSubmit}>
+            Submit signed
           </button>
         </form>
       </header>
--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const server = express();
 server.use(express.json());
+server.use(fileUpload({
+  useTempFiles: true
+}));
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: 'dbl8pi1ms',
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'hi' });
+});
+
+server.post('/upload', (req, res) => {
+  const file = req.files.image;
+  console.log(file);
+  cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: 'something broke' });
+    }
+
+    res.status(201).json({
+      success: true,
+      result
+    });
+  });
+
+  //res.status(201).json({ message: 'received' });
 });
 
 module.exports = server;
